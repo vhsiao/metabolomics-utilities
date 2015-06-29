e2eb97874f362454567bdafb79f50ae36004556c
Vivian Hsiao <vhsiao@MacBook-Pro.local>
2015-06-29 13:40:31 -0400

    name map utils passed unit tests

--- name_map_utils/__init__.py
+++ name_map_utils/__init__.py
@@ -17,7 +17,14 @@ class NameMap():
     def __contains__(self, item):
         return self.name_map.__contains__(item)
 
-    def add_mappings(self, name_map_csv_path=None):
+    def __len__(self):
+        return self.name_map.__len__()
+
+    def clear(self):
+        self.name_map = dict()
+        self._commit()
+
+    def add_mappings(self, name_map_csv_path):
         """
         :param name_map_csv_path: path/to/a CSV file containing mappings to add to the NameMap
         :return: None
@@ -25,17 +32,20 @@ class NameMap():
         update_method = lambda input_name_map: self.name_map.update(input_name_map)
         self._update_mappings(update_method, name_map_csv_path)
 
-    def remove_mappings(self, name_map_csv_path=None):
+    def remove_mappings(self, name_map_csv_path):
         """
         :param name_map_csv_path: path/to/a CSV file containing mappings to remove from the NameMap
         :return: None
         """
-        update_method = lambda input_name_map: filter(lambda x: x not in input_name_map, self.name_map)
+        def update_method(input_name_map):
+            wanted_keys = filter(lambda k: (k not in input_name_map)
+                                 or (k in input_name_map and not input_name_map[k] == self.name_map[k]), self.name_map)
+            self.name_map = {k: self.name_map[k] for k in wanted_keys}
         self._update_mappings(update_method, name_map_csv_path)
 
-    def _update_mappings(self, update_method, name_map_csv_path=None):
+    def _update_mappings(self, update_method, name_map_csv_path):
         input_name_map = self.get_map_from_csv(name_map_csv_path)
-        self.name_map = update_method(input_name_map)
+        update_method(input_name_map)
         self._commit()
 
     def _commit(self):
@@ -43,8 +53,8 @@ class NameMap():
             pickle.dump(self.name_map, map_pkl)
 
     @classmethod
-    def get_map_from_csv(self, name_map_csv_path=None):
-        name_map_2 = None
+    def get_map_from_csv(self, name_map_csv_path):
+        name_map_2 = dict()
         try:
             with open(name_map_csv_path) as name_map_csv:
                 reader = csv.DictReader(name_map_csv)
@@ -53,17 +63,19 @@ class NameMap():
                 except:
                     print('Had trouble adding entries from this name_map file. Double check the file {0}'
                           .format(name_map_csv_path))
-        except:
-            print('No name map was given.')
+        except IOError:
+            print('Name map at path {0} missing or invalid. Returning empty map.'.format(name_map_csv_path))
+            raise
         return name_map_2
 
     def __init__(self, map_pkl_path='maps/map.pkl'):
         try:
             with open(map_pkl_path, 'w+') as map_pkl:
+                self.map_pkl_path = map_pkl_path
                 self.name_map = pickle.load(map_pkl)
+        except EOFError:
             self.map_pkl_path = map_pkl_path
-        except:
-            self.name_map = pickle.loads('')
+        except IOError:
             self.map_pkl_path = 'temp_map.pkl'
 
 if __name__ == 'main':
